4709cccc73df46dadda2a2202ee5c889f4ac9003
palakpadalia <padaliapalak19@gmail.com>
2023-05-29 12:41:26 +0530

    Enabled Old RTO API

--- migoo_crm/code_override/check_user_role.py
+++ migoo_crm/code_override/check_user_role.py
@@ -1,8 +1,10 @@
-import frappe 
+import frappe
+
 
 @frappe.whitelist(allow_guest=True)
 def role_check(user):
-    d=frappe.db.get_value('User',user,'role_profile_name')
+    d = frappe.db.get_value('User', user, 'role_profile_name')
     # d1=frappe.db.get_value('Supplier', filters={"email":user})
-    frappe.response["role"]=d
-    # frappe.response["supplier"]=d1 f 
+    frappe.response["role"] = d
+    # frappe.msgprint(d)
+    frappe.response["supplier"] = d
