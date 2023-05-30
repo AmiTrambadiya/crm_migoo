2d8c770683341344780b2537dd0e0f302a28b116
palakpadalia <padaliapalak19@gmail.com>
2023-05-30 14:57:33 +0530

    Dashboard Changes

---
+++ migoo_crm/code_override/check_user_role.py
@@ -0,0 +1,8 @@
+import frappe 
+
+@frappe.whitelist(allow_guest=True)
+def role_check(user):
+    d=frappe.db.get_value('User',user,'role_profile_name')
+    # d1=frappe.db.get_value('Supplier', filters={"email":user})
+    frappe.response["role"]=d
+    # frappe.response["supplier"]=d1 f 
